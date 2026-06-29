edec09fac9839eda12c3bcd3dc50a678789dc034
Jieun Hwang <hwangje36@gmail.com>
2026-06-29 15:41:24 +0900

    테스트 및 onnx 변환 시 weight_only=False로 변경

--- exps/example/custom/yolox_s.py
+++ exps/example/custom/yolox_s.py
@@ -19,13 +19,24 @@ class Exp(MyExp):
         self.val_ann = "val.json"
         self.input_size = (512, 640)
         self.test_size = (512, 640)
+        self.multiscale_range = 0
         
         self.num_classes = 1
 
-        self.max_epoch = 5
+        self.max_epoch = 100
         self.data_num_workers = 4
-        self.eval_interval = 1
+        self.eval_interval = 10
         self.no_aug_epochs = 15
         
         self.save_history_ckpt = False
         self.print_interval = 100
+        
+        self.test_conf = 0.001
+        self.nmsthre = 0.75
+
+        self.mosaic_prob = 1.0
+        self.mixup_prob = 0.0
+        self.degrees = 30.0
+        self.translate = 0.1
+        self.scale = (0.5, 1.5)
+        self.shear = 2.0
